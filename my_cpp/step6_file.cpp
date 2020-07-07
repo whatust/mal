44042bf0fa9f65767ab903051f7edaa61af53921
whatust <whatust@gmail.com>
2020-07-07 17:52:04 -0300

    Added load-file operator

--- my_cpp/step6_file.cpp
+++ my_cpp/step6_file.cpp
@@ -23,6 +23,7 @@ int main(int argc, char* argv[]) {
     MalEnv repl_env;
     start_outer_env(repl_env);
     rep(std::string("(def! not(fn* (a) (if a false true)))"), repl_env);
+    rep(std::string("(def! load-file (fn* (f) (eval (read-string (str \"(do \" (slurp f) \"\nnil)\")))))"), repl_env);
 
      while(readLine.read(prompt, input)) {
 
